26c0b7429227c019ffa2025800c6b7c5e8936426
cjting <cjting@gmail.com>
2013-03-16 15:55:33 -0700

    save changes to add REST API and DB helper methods

--- app.js
+++ app.js
@@ -56,7 +56,9 @@ function onConnectSuccessful(db) {
             });
     });
 
-
+    //====================================
+    // CARDS REST API
+    //====================================
     app.get('/cards', function(req, res) {
         dbHelper.getAllCards(db)
             .done(function(cardsList) {
@@ -84,7 +86,7 @@ function onConnectSuccessful(db) {
 
     app.post('/card', function(req,res) {
         var card = {};
-        card._id = req.body.id;
+        //card._id = req.body.id;
         card.userid = req.body.userid;
         card.question = req.body.question;
         card.answer = req.body.answer;
@@ -144,6 +146,185 @@ function onConnectSuccessful(db) {
             res.send(err);
         });
     });
+
+    //====================================
+    // DECKS REST API
+    //====================================
+
+    app.get('/deck', function(req, res) {
+        dbHelper.getAllDecks(db)
+            .done(function(decksList) {
+                res.send(decksList);
+
+            })
+            .fail(function(err) {
+                res.send(err);
+            });
+    });
+
+    app.get('/deck/:id', function(req, res) {
+        dbHelper.getDeck(db, req.params.id)
+            .done(function(deck) {
+                res.send(deck);
+            })
+            .fail(function(err) {
+                res.send(err);
+            });
+    });
+
+    app.post('/deck', function(req,res) {
+
+        var deck = {};
+        deck.owner_id = req.body.owner_id;
+        deck.name = req.body.name;
+        deck.card_ids = req.body.card_ids;
+        deck.creation_ts = new Date();
+        deck.last_modified = new Date();
+        deck.status = 'active';
+
+        dbHelper.createDeck(db, deck)
+            .done(function(deck) {
+                console.log('New deck created.')
+                res.send(deck);
+            })
+            .fail(function(err) {
+                console.log('Failed to create new deck')
+                res.send(err);
+            });
+    });
+
+    app.put('/deck/:id', function(req,res) {
+        var deck = {};
+        var id = req.params.id;
+
+        if(req.body.owner_id) { deck.owner_id = req.body.owner_id; }
+        if(req.body.name) { deck.name = req.body.name; }
+        if(req.body.card_ids) { deck.card_ids = req.body.card_ids; }
+        if(req.body.status) { user.status = req.body.status; }
+        deck.last_modified = new Date();
+
+        dbHelper.updateDeck(db, id, card)
+            .done(function(card) {
+                console.log('Deck id:' + req.params.id+ ' updated.');
+                res.send(card);
+            })
+            .fail(function(err) {
+                console.log('Failed to update deck')
+                res.send(err);
+            });
+
+    });
+
+
+    app.delete('/deck/:id', function(req,res) {
+        var id = req.params.id;
+
+        dbHelper.deleteDeck(db, id)
+            .done(function(deck) {
+                console.log('Deck id:' + id + ' deleted.');
+                res.send({'status':'ok'});
+            })
+            .fail(function(err) {
+                console.log('Failed to delete deck')
+                res.send(err);
+            });
+    });
+
+
+
+    //====================================
+    // USERS REST API
+    //====================================
+
+    app.get('/user', function(req, res) {
+        dbHelper.getAllUsers(db)
+            .done(function(usersList) {
+                res.send(usersList);
+
+            })
+            .fail(function(err) {
+                res.send(err);
+            });
+    });
+
+    app.get('/user/:id', function(req, res) {
+        dbHelper.getUser(db, req.params.id)
+            .done(function(user) {
+                //res.render('list', {
+                //    list : list
+                //});
+                res.send(user);
+
+            })
+            .fail(function(err) {
+                res.send(err);
+            });
+    });
+
+    app.post('/user', function(req,res) {
+
+        var email = req.body.email;
+
+        var user = {};
+        user.email = email;
+        user.userid = (email) ? email.substring(0, email.indexOf("@")) : "";
+        user.pwd = req.body.pwd;
+        user.points = req.body.points;
+        user.deck_ids = req.body.deck_ids;
+        user.creation_ts = new Date();
+        user.last_modified = new Date();
+        user.status = 'active';
+
+        dbHelper.createUser(db, user)
+            .done(function(user) {
+                console.log('New user created.')
+                res.send(user);
+            })
+            .fail(function(err) {
+                console.log('Failed to create new user')
+                res.send(err);
+            });
+    });
+
+    app.put('/user/:id', function(req,res) {
+        var user = {};
+        var id = req.params.id;
+
+        if(req.body.email) { user.email = req.body.email; }
+        if(req.body.pwd) { user.question = req.body.pwd; }
+        if(req.body.points) { user.points = req.body.points; }
+        if(req.body.deck_ids) { user.deck_ids = req.body.deck_ids; }
+        if(req.body.status) { user.status = req.body.status; }
+
+        card.last_modified = new Date();
+
+        dbHelper.updateUser(db, id, card)
+            .done(function(card) {
+                console.log('Card id:' + req.params.id+ ' updated.');
+                res.send(card);
+            })
+            .fail(function(err) {
+                console.log('Failed to update card')
+                res.send(err);
+            });
+
+    });
+
+
+    app.delete('/user/:id', function(req,res) {
+        var id = req.params.id;
+
+        dbHelper.deleteUser(db, id)
+            .done(function(user) {
+                console.log('User id:' + id + ' deleted.');
+                res.send({'status':'ok'});
+            })
+            .fail(function(err) {
+                console.log('Failed to delete user')
+                res.send(err);
+            });
+    });
+
 
     // Catches every 404
     app.use(function(req, res, next) {
--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 var mongo = require('mongodb'),
+    BSON = mongo.BSONPure,
     Server = mongo.Server,
     Db = mongo.Db,
     Deferred = require('Deferred');
@@ -18,13 +19,24 @@ var apis = {
             err ? deferred.reject(err) : deferred.resolve(db);
         });
 
+        db.createCollection('users', function(err, collection) {
+            console.log('users collection created')
+        });
+
         db.createCollection('cards', function(err, collection) {
             console.log('cards collection created')
+        });
+
+        db.createCollection('decks', function(err, collection) {
+            console.log('decks collection created')
         });
 
         return deferred;
     },
 
+    //========================================
+    // CARDS DB API's
+    //========================================
     getAllCards : function(db) {
         var deferred = Deferred();
 
@@ -107,19 +119,19 @@ var apis = {
         return deferred;
     },
 
-    //TODO: Change to do an upsert instead of an update
     updateCard : function(db, id, card) {
         var deferred = Deferred();
 
         db.collection('cards', function(err, collection) {
 
+            var o_id = new BSON.ObjectID(id);
             var query = {
-                _id : id
+                _id : o_id
             };
 
             if(!err) {
-
-                collection.findAndModify(query, [['_id','asc']], {$set: card}, {new:true}, function(err, updatedCard) {
+                //collection.findAndModify(query, [['_id','asc']], {$set: card}, {new:true}, function(err, updatedCard) {
+                collection.update(query, card, {safe:true, upsert:true}, function(err, updatedCard) {
                     if(!err) { deferred.resolve(updatedCard);}
                     else {
                         console.warn(err.message);
@@ -139,6 +151,7 @@ var apis = {
         return deferred;
     },
 
+    // Do a soft delete. Just set the flag. Not actually removing record from DB
     deleteCard : function(db, id) {
         var deferred = Deferred();
 
@@ -200,8 +213,277 @@ var apis = {
         });
 
         return deferred;
-    }
+    },
 
+    //========================================
+    // USERS DB API's
+    //========================================
+    getUser: function(db, id) {
+        var deferred = Deferred();
+
+        db.collection('users', function(err, collection) {
+
+            var query = {
+                _id : id
+            };
+
+            if(!err) {
+                collection.findOne(query, function(err, user) {
+                    if(err || !user) {
+                        deferred.reject('No user record found');
+                    }
+                    else {
+                        // Found the document
+                        deferred.resolve(user);
+                    }
+                });
+            }
+            else {
+                deferred.reject('Cannot connect to users collection');
+            }
+        });
+
+        return deferred;
+    },
+
+    createUser : function(db, user) {
+        var deferred = Deferred();
+
+        db.collection('users', function(err, collection) {
+
+            if(!err) {
+
+                collection.insert(user, {safe:true}, function(err, records) {
+                    if(!err) { deferred.resolve(records);}
+                    else {
+                        console.warn(err.message);
+                        deferred.reject('Error creating user');
+
+                        if (err && err.message.indexOf('E11000 ') !== -1) {
+                            // this _id was already inserted in the database
+                        }
+                    }
+                });
+            }
+            else {
+                deferred.reject('Cannot connect to users collection');
+            }
+        });
+
+        return deferred;
+    },
+
+    updateUser : function(db, id, user) {
+        var deferred = Deferred();
+
+        db.collection('users', function(err, collection) {
+
+            var o_id = new BSON.ObjectID(id);
+            var query = {
+                _id : o_id
+            };
+
+            if(!err) {
+                //collection.findAndModify(query, [['_id','asc']], {$set: card}, {new:true}, function(err, updatedCard) {
+                collection.update(query, user, {safe:true, upsert:true}, function(err, updatedUser) {
+                    if(!err) { deferred.resolve(updatedUser);}
+                    else {
+                        console.warn(err.message);
+                        deferred.reject('Error updating user: ' + user.email);
+
+                        if (err && err.message.indexOf('E11000 ') !== -1) {
+                            // this _id was already inserted in the database
+                        }
+                    }
+                });
+            }
+            else {
+                deferred.reject('Cannot connect to cards collection');
+            }
+        });
+
+        return deferred;
+    },
+
+    deleteUser : function(db, id) {
+        var deferred = Deferred();
+
+        db.collection('users', function(err, collection) {
+
+            var query = {
+                _id : id
+            };
+
+            if(!err) {
+
+                collection.findAndModify(query, [['_id','asc']], {$set: {'status':'deleted'}}, {new:true}, function(err, updatedCard) {
+                    if(!err) { deferred.resolve(updatedCard);}
+                    else {
+                        console.warn(err.message);
+                        deferred.reject('Error deleting user');
+
+                        if (err && err.message.indexOf('E11000 ') !== -1) {
+                            // this _id was already inserted in the database
+                        }
+                    }
+                });
+            }
+            else {
+                deferred.reject('Cannot connect to users collection');
+            }
+        });
+
+        return deferred;
+    },
+
+    //========================================
+    // DECKS DB API's
+    //========================================
+
+    getAllDecks : function(db) {
+        var deferred = Deferred();
+
+        db.collection('decks', function(err, collection) {
+
+            if(!err) {
+                var cursor = collection.find({}, function(err, card) {
+                    if(err || !card) {
+                        deferred.reject('No documents found');
+                    }
+                });
+
+                var decksList = [];
+                cursor.each(function(item) {
+                    decksList.push(item);
+                });
+                deferred.resolve(decksList);
+
+            }
+            else {
+                deferred.reject('Cannot connect to decks collection');
+            }
+        });
+
+        return deferred;
+    },
+
+    getDeck: function(db, id) {
+        var deferred = Deferred();
+
+        db.collection('decks', function(err, collection) {
+
+            var query = {
+                _id : id
+            };
+
+            if(!err) {
+                collection.findOne(query, function(err, user) {
+                    if(err || !user) {
+                        deferred.reject('No deck record found');
+                    }
+                    else {
+                        // Found the document
+                        deferred.resolve(user);
+                    }
+                });
+            }
+            else {
+                deferred.reject('Cannot connect to decks collection');
+            }
+        });
+
+        return deferred;
+    },
+
+    createDeck : function(db, deck) {
+        var deferred = Deferred();
+
+        db.collection('decks', function(err, collection) {
+
+            if(!err) {
+
+                collection.insert(deck, {safe:true}, function(err, records) {
+                    if(!err) { deferred.resolve(records);}
+                    else {
+                        console.warn(err.message);
+                        deferred.reject('Error creating deck');
+
+                        if (err && err.message.indexOf('E11000 ') !== -1) {
+                            // this _id was already inserted in the database
+                        }
+                    }
+                });
+            }
+            else {
+                deferred.reject('Cannot connect to decks collection');
+            }
+        });
+
+        return deferred;
+    },
+
+    updateDeck : function(db, id, deck) {
+        var deferred = Deferred();
+
+        db.collection('decks', function(err, collection) {
+
+            var o_id = new BSON.ObjectID(id);
+            var query = {
+                _id : o_id
+            };
+
+            if(!err) {
+                //collection.findAndModify(query, [['_id','asc']], {$set: card}, {new:true}, function(err, updatedCard) {
+                collection.update(query, user, {safe:true, upsert:true}, function(err, updatedDeck) {
+                    if(!err) { deferred.resolve(updatedUser);}
+                    else {
+                        console.warn(err.message);
+                        deferred.reject('Error updating deck: ' + deck.id);
+
+                        if (err && err.message.indexOf('E11000 ') !== -1) {
+                            // this _id was already inserted in the database
+                        }
+                    }
+                });
+            }
+            else {
+                deferred.reject('Cannot connect to decks collection');
+            }
+        });
+
+        return deferred;
+    },
+
+    deleteDeck : function(db, id) {
+        var deferred = Deferred();
+
+        db.collection('decks', function(err, collection) {
+
+            var query = {
+                _id : id
+            };
+
+            if(!err) {
+
+                collection.findAndModify(query, [['_id','asc']], {$set: {'status':'deleted'}}, {new:true}, function(err, updatedDeck) {
+                    if(!err) { deferred.resolve(updatedDeck);}
+                    else {
+                        console.warn(err.message);
+                        deferred.reject('Error deleting deck');
+
+                        if (err && err.message.indexOf('E11000 ') !== -1) {
+                            // this _id was already inserted in the database
+                        }
+                    }
+                });
+            }
+            else {
+                deferred.reject('Cannot connect to decks collection');
+            }
+        });
+
+        return deferred;
+    }
 
 };
 
